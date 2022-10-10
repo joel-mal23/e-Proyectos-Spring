115fb5d557882a38a6c9923d4bb46e9b6323ed06
joel-mal23 <joel.malvaez.23@gmail.com>
2022-10-10 11:28:55 -0500

    config-server

---
+++ config-server/src/test/java/arquitectura/micro/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitectura.micro.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
