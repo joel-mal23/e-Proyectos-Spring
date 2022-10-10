1ac59bab5aa762cde5c9b1f1c6e36617d382afd0
joel-mal23 <joel.malvaez.23@gmail.com>
2022-10-10 13:05:02 -0500

    eureka

--- clientes/src/main/java/arquitectura/micros/clientes/Clientes/ClientesApplication.java
+++ clientes/src/main/java/arquitectura/micros/clientes/Clientes/ClientesApplication.java
@@ -2,8 +2,10 @@ package arquitectura.micros.clientes.Clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientesApplication {
 
 	public static void main(String[] args) {
